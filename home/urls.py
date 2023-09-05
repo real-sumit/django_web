9f4edbad8db225265c57c31a7c489b14de26a916
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-09-05 12:08:10 +0530

    authentication system added

--- home/urls.py
+++ home/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path("test", views.test, name="test"),
     path("about", views.about, name="about"),
     path("services", views.services, name="services"),
-    path("contact", views.contact, name="contact")
+    path("contact", views.contact, name="contact"),
+    path("login", views.loginUser, name="login"),
+    path("logout", views.logoutUser, name="logout")
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from datetime import datetime
 from .models import Person
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
 
 def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
     return render(request, "index.html")
-    
+
 def about(request):
     return render(request, "about.html")
 
 def services(request):
     return render(request, "services.html")
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        # else:
+        #     return render(request, 'login.html')
+
+    return render(request, "login.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
 
 def contact(request):
     if request.method == "POST":
@@ -31,3 +52,6 @@ def test(request):
     }
     return render(request, "test.html", context)
     # return HttpResponse("Welcome to our homepage")
+
+# admin 
+# pass: admin
